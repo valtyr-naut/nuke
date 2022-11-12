56ddaa1ce06ac7233affd7e8659203b847197b7d
valtyr <valtyr.github@gmail.com>
2022-11-12 00:14:39 -0800

    core: dropped firefox

--- nuke.py
+++ nuke.py
@@ -1,41 +1,40 @@
-from selenium.webdriver.common.keys import Keys as keys
-from webbrowser import open
-from selenium import webdriver as wd
-from time import sleep
-from enum import Enum
 import json
+from enum import Enum
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys as keys
+
 import core
 
 BANNER:str=r"""
- ________   ___  ___  ___  __    _______      
-|\   ___  \|\  \|\  \|\  \|\  \ |\  ___ \     
-\ \  \\ \  \ \  \\\  \ \  \/  /|\ \   __/|    
- \ \  \\ \  \ \  \\\  \ \   ___  \ \  \_|/__  
-  \ \  \\ \  \ \  \\\  \ \  \\ \  \ \  \_|\ \ 
-   \ \__\\ \__\ \_______\ \__\\ \__\ \_______\
-    \|__| \|__|\|_______|\|__| \|__|\|_______|
-                                        valtyr
-"""
+ ________  ________  ________  ___  ________  ___               ________   ___  ___  ___  __    _______      
+|\   ____\|\   __  \|\   ____\|\  \|\   __  \|\  \             |\   ___  \|\  \|\  \|\  \|\  \ |\  ___ \     
+\ \  \___|\ \  \|\  \ \  \___|\ \  \ \  \|\  \ \  \            \ \  \\ \  \ \  \\\  \ \  \/  /|\ \   __/|    
+ \ \_____  \ \  \\\  \ \  \    \ \  \ \   __  \ \  \            \ \  \\ \  \ \  \\\  \ \   ___  \ \  \_|/__  
+  \|____|\  \ \  \\\  \ \  \____\ \  \ \  \ \  \ \  \____        \ \  \\ \  \ \  \\\  \ \  \\ \  \ \  \_|\ \ 
+    ____\_\  \ \_______\ \_______\ \__\ \__\ \__\ \_______\       \ \__\\ \__\ \_______\ \__\\ \__\ \_______\
+   |\_________\|_______|\|_______|\|__|\|__|\|__|\|_______|        \|__| \|__|\|_______|\|__| \|__|\|_______|
+   \|_________|                                                                                     by valtyr
+<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"""
 
-DEFAULT_CONFIG:str='{"init": true, "browser": {"chrome": false, "firefox": false}, "sites": {"reddit": false, "twitter": false}, "payload": {"posts": false, "comments": false}}'
+DEFAULT_CONFIG:str='{"init": true, "sites": {"reddit": false, "twitter": false}, "payload": {"posts": false, "comments": false}}'
 
-class site(Enum):
-    WEBPAGE_REDDIT:str = "https://www.reddit.com"
-    WEBPAGE_TWITTER:str = "https://twitter.com/home"
+class sites(Enum):
+    REDDIT:str = "https://www.reddit.com"
+    TWITTER:str = "https://twitter.com/home"
 
-class driver(Enum):
-    CHROME:str = "https://chromedriver.chromium.org/downloads"
-    FIREFOX:str = "https://github.com/mozilla/geckodriver/releases"
-
-class payload(Enum):
+class payloads(Enum):
     POSTS:str = "POSTS"
     COMMENTS:str = "COMMENTS"
 
-
-def choice():
+def nuke_reddit():
     ...
+    #the webdriver steps
 
-
+def nuke_twitter():
+    ...
+    #the webdriver steps
 
 if __name__=="__main__":
     print(BANNER)
@@ -43,21 +42,8 @@ if __name__=="__main__":
     config = json.loads(core.read(path="config.json",arg="r"))
 
     if config['init'] == True:
-        print("""Looks like this is your first time using this tool...\nSo here's how it works.\n\t- Select which browser you normally use from the menu.\n\t- Select which sites you want to nuke.\n\t- Select what you want to nuke.\n\t- Download and install the web driver from the window that opens after saving your choices.\nAll of these choices will be saved to config.json.\nYou can reinitialize these choices from the menu.\nUse responsibly.\n""")
-        print("What browser do you use?")
-        print("[0] Chrome")
-        print("[1] Firefox")
-        while True:
-            choice=input('< ')
-            if choice == '0':
-                config["browser"]["chrome"]=True
-                break
-            elif choice == '1':
-                config["browser"]["firefox"]=True
-                break
-            else:
-                print('Unexpected choice.')
-        
+        print("""Looks like this is your first time using this tool...\nSo here's how it works.\n\t- Select which sites you want to nuke.\n\t- Select what you want to nuke.\nAll of these choices will be saved to config.json.\nYou can reinitialize these choices from the menu.\nUse responsibly.\n""")
+         
         print("What site do you want to nuke?")
         print("[0] Reddit")
         print("[1] Twitter")
@@ -99,29 +85,50 @@ if __name__=="__main__":
         print('Saving choices...')
         config["init"]=False
         print("Choices saved!", end='\n') if core.write(path='config.json',arg='w',data=json.dumps(config)) else print('Choices not saved!', end='\n')
-        print('Install the required driver.')
-        open(driver.CHROME.value) if config["browser"]["chrome"]==True else open(driver.FIREFOX.value)
 
-    while True:    
-        print("<><><><><><><><><><><><><><><><>")
+    while True: 
+        nuking:bool=False   
         print("[0] nuke")
         print("[1] show config")
-        print("[2] open webdriver download page")
-        print("[3] reinitialize choices")
+        print("[2] reinitialize choices")
         print("[-] exit",end="\n")
         choice=input('< ')
         if choice == '0':
-            ...
+
+            #TODO DISPLAY OPTIONS AND CONFIRM NUKE
+            
+            #Maybe add firefox in future
+            # if config['browser']['chrome'] == True else webdriver.Firefox(executable_path="F:/valtyr/nuke/drivers/geckodriver.exe")
+
+            #DRIVE SEPERATE INSTANCES
+            #TODO get full path from os
+
+            print("Nuking...")
+            nuking=True
+
+            if config['sites']['reddit'] == True:
+                driver_reddit = webdriver.Chrome("F:/valtyr/nuke/drivers/chromedriver.exe") 
+                driver_reddit.get(sites.REDDIT.value) 
+            if config['sites']['twitter'] == True:
+                driver_twitter = webdriver.Chrome("F:/valtyr/nuke/drivers/chromedriver.exe") 
+                driver_twitter.get(sites.TWITTER.value) 
+
+                #NUKE HERE
+           
         elif choice == '1':
             print(config)
         elif choice == '2':
-            open(driver.CHROME.value) if config["browser"]["chrome"]==True else open(driver.FIREFOX.value)
-        elif choice == '3':
-            print("Choices reset!",end='\n') if core.write(path='config.json',arg='w',data=DEFAULT_CONFIG) else print('Choices not reset!', end='\n')
+            core.write(path='config.json',arg='w',data=DEFAULT_CONFIG)
+            print('Choices reset!')
+            print('Restart required.')
+            exit()
         elif choice == '-':
             exit()
         else:
             print('Unexpected input.')
+
+        if nuking:
+            break
 
 
         
